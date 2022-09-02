10f1fdc9fd629d5981937305d475dc8a789f67d2
gabo <gabriel.garcia@sovos.com>
2022-09-02 17:18:01 -0300

    Adding Xpath and a method for the add to cart button

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,15 +3,21 @@
 import { LoginPage } from "../support/Pages/loginPage";
 import { RegistroPage } from "../support/Pages/registerPage";
 import { HomePage } from "../support/Pages/homePage";
+import { onlineShopPage } from "../support/Pages/onlineShopPage";
+
 describe('Pre Entrega', () => {
-  let datosLogin;
+  let datosLogin,productos;
   const loginPage= new LoginPage();
   const registerPage= new RegistroPage();
   const homePage = new HomePage();
+  const onlineShop=new onlineShopPage();
 
   before('Set Up',()=>{
     cy.fixture("loginData").then(datos=>{
       datosLogin=datos;
+    })
+    cy.fixture("products").then(prods=>{
+      productos=prods;
     })
   })
   beforeEach("Before Each",()=>{
@@ -22,5 +28,9 @@ describe('Pre Entrega', () => {
 
   it('Hacer click en show total price y verificar el precio acumulado de 2 productos', () => {
     homePage.clickOnlineShop()
+    onlineShop.checkUrl()
+    onlineShop.clickOnAddToCart(productos.PrimerProducto.nombre,productos.PrimerProducto.precio)
+
+
   })
 })
--- cypress/support/Pages/onlineShopPage.js
+++ cypress/support/Pages/onlineShopPage.js
@@ -1,11 +1,14 @@
 export class onlineShopPage{
     constructor(){
         this.url = "home/onlineshop"
+        this.addToCartButton = "//button[text()='Add to cart']"
     }
     checkUrl(){
         cy.url().should('include',this.url)
     }
-
+    clickOnAddToCart(product,precio){
+        cy.get(`//button[text()='Add to cart' and @name=${precio} and @value='${product}']`).click()
+    }
  
 
 }
