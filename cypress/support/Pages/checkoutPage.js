8f17bbe8cd95252edd7a18a8ccb21f70e3b1f902
gabo <gabriel.garcia@sovos.com>
2022-09-18 16:35:34 -0300

    crear clase para checkout page

---
+++ cypress/support/Pages/checkoutPage.js
@@ -0,0 +1,10 @@
+export class CheckoutPage{
+    constructor(){
+        this.firstName = "[name=firstName]",
+        this.lastName = "[name=lastName]",
+        this.cardNumber = "[name=cardNumber]"
+    }
+
+    
+
+}
